a85f786d155c91aec9f60182d48e1f2dbe68b74c
André Schlichting <andresch.dev@gmail.com>
2026-04-01 18:10:19 -0300

    Add header responsivity

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,8 @@ languageSelector?.addEventListener("click", function toggleDisplayOfLanguageModa
   } else {
     languageModal?.classList.add("open");
   }
+
+  menuModal?.classList.remove("open");
 });
 
 for (const button of languageModalButtons) {
@@ -40,6 +42,36 @@ for (const button of languageModalButtons) {
     languageModal?.classList.remove("open");
   });
 }
+
+/* Menu */
+
+const menuButton = document.querySelector(".menu-button");
+const menuModal = document.querySelector(".menu-modal");
+const menuModalLinks = document.querySelectorAll(".menu-modal a");
+
+menuButton?.addEventListener("click", function toggleDisplayOfMenuModal() {
+  if (menuModal?.classList.contains("open")) {
+    menuModal?.classList.remove("open");
+  } else {
+    menuModal?.classList.add("open");
+  }
+
+  languageModal?.classList.remove("open");
+});
+
+window.addEventListener("resize", function closeMenuModalOnResize() {
+  if (window.innerWidth > 768) {
+    menuModal?.classList.remove("open");
+  }
+});
+
+for (const link of menuModalLinks) {
+  link.addEventListener("click", function closeMenuModal() {
+    menuModal?.classList.remove("open");
+  });
+}
+
+/* Icons */
 
 const arrowIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="var(--text)" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-down-icon lucide-chevron-down"><path d="m6 9 6 6 6-6"/></svg>`;
 
